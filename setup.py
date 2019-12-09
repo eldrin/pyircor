948cb2daf0fbdb3537ac1ad8f1b681891f74cc17
Jaehun Kim <j.h.kim@tudelft.nl>
2019-12-09 17:14:18 +0100

    downgraded some pakcages and limited the python3.8

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [ ]
 setup(
     author="Jaehun Kim",
     author_email='jaehun.j.kim@gmail.com',
-    python_requires='>=3.5',
+    python_requires='>=3.5, <3.8',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
@@ -33,7 +33,6 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
     ],
     description="Python implementation of the R package `ircor`",
     # entry_points={
