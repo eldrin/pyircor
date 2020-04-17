dc372e13c3f2e288491afca8a3efcbae947fcf5b
Jaehun Kim <j.h.kim@tudelft.nl>
2020-04-17 22:10:31 +0200

    fix typo on setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [ ]
 setup(
     author="Jaehun Kim",
     author_email='jaehun.j.kim@gmail.com',
-    python_requires='>=3.5, <3.8',
+    python_requires='>=3.6, <=3.8',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
