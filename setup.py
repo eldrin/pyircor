d77e56e897bc7e65ff98ef53938ef8645bae689c
Jaehun Kim <j.h.kim@tudelft.nl>
2020-04-17 22:02:56 +0200

    updated py3 version supports)

--- setup.py
+++ setup.py
@@ -30,9 +30,9 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     description="Python implementation of the R package `ircor`",
     # entry_points={
