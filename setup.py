c60c1401bfcc66fe563ba833061db0275b6b06f5
Jaehun Kim <j.h.kim@tudelft.nl>
2020-04-17 22:08:20 +0200

    dropped 3.5, added 3.8

--- setup.py
+++ setup.py
@@ -23,16 +23,16 @@ test_requirements = [ ]
 setup(
     author="Jaehun Kim",
     author_email='jaehun.j.kim@gmail.com',
-    python_requires='>=3.5, <3.8',
+    python_requires='>=3.6, <=3.8',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     description="Python implementation of the R package `ircor`",
     # entry_points={
